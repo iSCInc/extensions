1c1ebb8d8b805d47dd402d4c869ba9882c006931
Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com>
2015-07-14 14:30:01 +0200

    Create versioncompatibility.php

---
+++ v1.24/V/VC/v1.0.0/versioncompatibility.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Version compatiblity (for iSC Inc. wikis)
+ * 
+ * @author Suriyaa Kudo <SuriyaaKudoIsc@users.noreply.github.com> (http://bit.ly/Suriyaa)
+ * @author MediaWiki contributors (https://www.mediawiki.org/wiki/Thread:Project:Support_desk/Set_version_compatibility_for_extension)
+ * 
+ */
+ 
+if ( version_compare( $GLOBALS['wgVersion'], '1.19', '<' ) ) {
+    echo 'myExt requires at least MediaWiki 1.19, you have ' . $GLOBALS['wgVersion'];
+    exit();
+}
+ 
+// Rest of code below!!!
